4c1841ea6417f235aef7f414b472c2b9545ccdce
Jerry Guo <jerryguowei@gmail.com>
2018-03-17 10:39:03 +1100

    arrays

---
+++ src/arrays/Generated.java
@@ -0,0 +1,14 @@
+package arrays;
+
+import generics.Generator;
+
+public class Generated {
+  public static <T> T[] array(T[] a, Generator<T> gen){
+	  return new CollectionData<T> (gen,a.length).toArray(a);
+  }
+  @SuppressWarnings("unchecked")
+  public static <T> T[] array(Class<T> type,Generator<T> gen,int size){
+	T[] a=(T[]) java.lang.reflect.Array.newInstance(type, size);
+	  return new CollectionData<>(gen, size).toArray(a);
+  }
+}
