0d811c13339319b9f292b0039599dbad77816663
Jerry Guo <jerryguowei@gmail.com>
2018-03-18 09:03:21 +1100

    containers

---
+++ src/arrays/Reverse.java
@@ -0,0 +1,15 @@
+package arrays;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Reverse {
+   public static void main(String[] args){
+	   CompType[] a=Generated.array(new CompType[12], CompType.generator());
+	   System.out.println("before sorting");
+	   System.out.println(Arrays.toString(a));
+	   Arrays.sort(a,Collections.reverseOrder());
+	   System.out.println("After sorting:");
+	   System.out.println(Arrays.toString(a));
+   }
+}
