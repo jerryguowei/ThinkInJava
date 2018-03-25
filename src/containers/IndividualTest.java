440f2fbb6be11927b11025ea84b6c3b8481fbe6b
Jerry Guo <jerryguowei@gmail.com>
2018-03-25 21:26:13 +1100

    map

---
+++ src/containers/IndividualTest.java
@@ -0,0 +1,10 @@
+package containers;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class IndividualTest {
+    public static void main(String[] args){
+    	Set<Individual> pets=new TreeSet<Individual>();
+    }
+}
