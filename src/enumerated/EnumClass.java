f5a851e1c1fc79b679f7939e9cd533d6617af476
Jerry Guo <jerryguowei@gmail.com>
2018-04-05 07:14:04 +1000

    enum

---
+++ src/enumerated/EnumClass.java
@@ -0,0 +1,16 @@
+package enumerated;
+
+enum Shrubbery{GROUND,CRAWLING,HAVING}
+public class EnumClass {
+    public static void main(String[] args){
+    	for(Shrubbery s:Shrubbery.values()){
+    		System.out.println(s + " ordinal: " + s.ordinal());
+    		System.out.print(s.compareTo(Shrubbery.CRAWLING)+ " ");
+    		System.out.print(s.equals(Shrubbery.CRAWLING)+ " ");
+    		System.out.println(s==Shrubbery.CRAWLING);
+    		System.out.println(s.getDeclaringClass());
+    		System.out.println(s.name());
+    		System.out.println("------------------");
+    	}
+    }
+}
